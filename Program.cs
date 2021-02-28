e5aa68b30733012061f086dafca424e720985610
Dave Morse <dcmorse@gmail.com>
2021-02-27 19:45:05 -0500

    .

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace phial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumericTile t = new NumericTile(2, true);
+            EyeTile e = new EyeTile();
+            FellowshipSpecialTile phial = new FellowshipSpecialTile(-2);
+            TheRingIsMine theRingIsMine = new TheRingIsMine();
+            ShadowSpecialNumericTile onOnTheyWent = new ShadowSpecialNumericTile(3, false);
+            ShadowSpecialNumericTile weShallGetIt = new ShadowSpecialNumericTile(1, true);
+            ShelobsLair shelobsLair = new ShelobsLair();
+            Console.WriteLine(t);
+            Console.WriteLine(e);
+            Console.WriteLine(phial);
+            Console.WriteLine(theRingIsMine);
+            Console.WriteLine(onOnTheyWent);
+            Console.WriteLine(weShallGetIt);
+            Console.WriteLine(shelobsLair);
+        }
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,117 @@
+namespace phial
+{
+    abstract class Tile
+    {
+        public Tile(bool reveal) {
+            _Reveal = reveal;
+        }
+        public abstract int Value(int huntHits);
+        private bool _Reveal { get; }
+        public bool Reveal() {
+            return _Reveal;
+        }
+        public virtual bool Stop() {
+            return false;
+        }
+        public virtual bool isEye() {
+            return false;
+        }
+        public virtual bool isSpecial() {
+            return false;
+        }
+        public virtual bool isFellowshipSpecial() {
+            return false;
+        }
+        public virtual bool isShadowSpecial() {
+            return false;
+        }
+        public override string ToString() {
+            return $"{GetType().Name} {ToStringSuffix()}";
+        }
+        public string ToStringSuffix() {
+            return $"{(Reveal() ? "r" : "")}{(Stop() ? " stop" : "")}";
+        }
+    }
+
+    class NumericTile : Tile
+    {
+        public NumericTile(int n, bool reveal) : base(reveal)
+        {
+            Number = n;
+        }
+        public int Number { get; }
+        public override int Value(int huntHits) {
+            return Number;
+        }
+        public override string ToString() {
+            return $"{GetType().Name} {Number}{ToStringSuffix()}";
+        }
+    }
+
+    class EyeTile : Tile
+    {
+        public EyeTile() : base(true) {}
+        public override bool isEye() {
+            return true;
+        }
+        public override int Value(int huntHits) {
+            return huntHits;
+        }
+    }
+
+    class FellowshipSpecialTile : NumericTile {
+        public FellowshipSpecialTile(int n) : base(n, false) {}
+        public override bool isSpecial() {
+            return true;
+        }
+        public override bool isFellowshipSpecial() {
+            return true;
+        }
+    }
+
+    class TheRingIsMine : EyeTile {
+        public override bool isSpecial() {
+            return true;
+        }
+        public override bool isShadowSpecial() {
+            return true;
+        }
+        public override bool Stop() {
+            return true;
+        }
+
+        public override string ToString() {
+            return $"{GetType().Name} Eye {ToStringSuffix()}";
+        }
+    }
+
+    class ShadowSpecialNumericTile : NumericTile {
+        public ShadowSpecialNumericTile(int n, bool reveal) : base(n, reveal) {}
+        public override bool isSpecial() {
+            return true;
+        }
+        public override bool isShadowSpecial() {
+            return true;
+        }
+        public override bool Stop() {
+            return true;
+        }
+    }
+
+
+    class ShelobsLair : Tile {
+        public ShelobsLair() : base(false) {}
+        public override bool isSpecial() {
+            return true;
+        }
+        public override bool isShadowSpecial() {
+            return true;
+        }
+        public override bool Stop() {
+            return true;
+        }
+        public override int Value(int huntHits) {
+            return 6; // TODO 1d6
+        }
+    }
+}
